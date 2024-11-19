2b1ca1eb741ff128aa8b673bc72cc25ed10f4185
BillyTan <www.billy39@gmail.com>
2024-11-19 11:07:12 +0700

    updated the ThreadRepository interface for functions verfifying thread availability

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -2,6 +2,9 @@ class ThreadRepository {
   async addThread(thread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+  async isThreadAvailable(id) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = ThreadRepository;
