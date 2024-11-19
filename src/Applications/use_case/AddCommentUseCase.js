fadf4fec86d62b9a7dac14654094471f5c332413
BillyTan <www.billy39@gmail.com>
2024-11-19 11:06:07 +0700

    updated the use case to handler add comment when thread is not available

--- src/Applications/use_case/AddCommentUseCase.js
+++ src/Applications/use_case/AddCommentUseCase.js
@@ -1,14 +1,21 @@
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
 const NewComment = require('../../Domains/comments/entities/NewComment');
 
 class AddCommentUseCase {
-  constructor({ commentRepository }) {
+  constructor({ commentRepository, threadRepository }) {
     this._commentRepository = commentRepository;
+    this._threadRepository = threadRepository;
 
     this.execute = this.execute.bind(this);
   }
 
   async execute(payload) {
     const newComment = new NewComment(payload);
+    console.log(newComment);
+    const isThreadAvailable = await this._threadRepository.isThreadAvailable(newComment.thread_id);
+    if (!isThreadAvailable) {
+      throw new NotFoundError('Thread tidak ditemukan');
+    }
     const addedComment = await this._commentRepository.addComment(newComment);
     return addedComment;
   }
--- src/Applications/use_case/_test/AddCommentUseCase.test.js
+++ src/Applications/use_case/_test/AddCommentUseCase.test.js
@@ -2,10 +2,38 @@ const AddCommentUseCase = require('../AddCommentUseCase');
 const NewComment = require('../../../Domains/comments/entities/NewComment');
 const CommentRepository = require('../../../Domains/comments/CommentRepository');
 const AddedComment = require('../../../Domains/comments/entities/AddedComment');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const NotFoundError = require('../../../Commons/exceptions/NotFoundError');
 
 jest.mock('../../../Domains/comments/entities/NewComment');
 
 describe('AddCommentUseCase', () => {
+  it('should throw not found error when the thread to add is not available', async () => {
+    // Arrange
+    const useCasePayload = {
+      thread_id: 'thread-unavailable',
+      content: 'Some Comment',
+      owner: 'user-123',
+    };
+
+    // mock NewComment constructor property
+    NewComment.mockImplementation((payload) => ({
+      thread_id: payload.thread_id,
+      content: payload.content,
+      owner: payload.owner,
+    }));
+
+    /** mock required depedencies */
+    const mockThreadRepository = new ThreadRepository();
+
+    /** mock required depedencies function */
+    mockThreadRepository.isThreadAvailable = jest.fn().mockResolvedValue(false);
+
+    const addCommentUseCase = new AddCommentUseCase({ commentRepository: {}, threadRepository: mockThreadRepository });
+
+    await expect(addCommentUseCase.execute(useCasePayload)).rejects.toThrowError(NotFoundError)
+  });
+
   it('should orchestrate add comment to threat correctly', async () => {
     // Arrange
     const useCasePayload = {
@@ -23,6 +51,7 @@ describe('AddCommentUseCase', () => {
 
     /** Mock required depedencies */
     const mockCommentRepository = new CommentRepository();
+    const mockThreadRepository = new ThreadRepository();
 
     /** Mock required depedencies function */
     mockCommentRepository.addComment = jest.fn().mockResolvedValue(
@@ -32,13 +61,15 @@ describe('AddCommentUseCase', () => {
         owner: 'user-123',
       })
     );
+    mockThreadRepository.isThreadAvailable = jest.fn().mockResolvedValue(true);
 
-    const addCommentUseCase = new AddCommentUseCase({ commentRepository: mockCommentRepository });
+    const addCommentUseCase = new AddCommentUseCase({ commentRepository: mockCommentRepository, threadRepository: mockThreadRepository });
 
     // Action
     const addedComment = await addCommentUseCase.execute(useCasePayload);
 
     expect(NewComment).toBeCalledWith(useCasePayload);
+    expect(mockThreadRepository.isThreadAvailable).toBeCalledWith('thread-123');
     expect(mockCommentRepository.addComment).toBeCalledWith(expect.objectContaining({
       thread_id: 'thread-123',
       content: 'Some Comment',
